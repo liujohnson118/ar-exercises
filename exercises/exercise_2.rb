d33488b2f67a481b9fa22a8d2be9707afa37e525
liujohnson118 <liujohnson118@yahoo.com>
2017-07-04 19:46:37 +0000

    done e4

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,7 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1=Store.find_by(id:1)
+@store2=Store.find_by(id:2)
+@store1.update(name:'Nanjing')
+puts "store1 name is now #{@store1.name}"
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,6 @@
 class Store < ActiveRecord::Base
-
+  validates:name, presence:true
+  validates:annual_revenue, presence:true
+  validates:mens_apparel, inclusion: { in: [true, false] }
+  validates:womens_apparel, inclusion: { in: [true, false] }
 end
